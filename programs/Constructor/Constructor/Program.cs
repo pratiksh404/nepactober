680206e734c701a297ab648ab4c585a50df80b30
Ayush kamacharya <40533133+ayus93@users.noreply.github.com>
2021-10-02 13:14:09 +0545

    salary

---
+++ programs/Constructor/Constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this school");
+            Console.WriteLine("----------------------------");
+            List <Student> Details = new List<Student>();
+            for(int i=0; i<5;i++)
+            {
+                Details.Add(new Student());
+                Console.WriteLine("Enter your name:");
+                Details[i].name = Console.ReadLine();
+                Console.WriteLine("Enter your age");
+                Details[i].age = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (Student odata in Details)
+            {
+                Console.WriteLine(odata.viewDetails());
+            }
+        }
+    }
+}
