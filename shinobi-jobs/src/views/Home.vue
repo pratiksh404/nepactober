38c929a59ea295aabb48922419e784b5093a7099
CoffeeCode404 <115384100+CoffeeCode404@users.noreply.github.com>
2022-10-09 08:52:13 +0545

    Shinobi jobs in VUE framework

---
+++ shinobi-jobs/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <br>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, aliquam? Odit reprehenderit dicta illo asperiores exercitationem velit quis voluptate veniam beatae, quae maxime, iste dolore, nulla minima porro quo totam fuga eveniet neque praesentium nobis! Placeat non pariatur suscipit. Officiis eveniet consequatur rerum magnam alias necessitatibus earum quidem incidunt soluta reprehenderit veniam, exercitationem nam explicabo dolor autem sunt eum ab est deserunt placeat. Odio est ducimus aliquam consectetur dolore mollitia ipsa minima, praesentium reiciendis vero necessitatibus nemo error quas facere animi voluptas cum deleniti, atque, earum enim ex? Ea dolorem incidunt nam consectetur id. Ullam dicta adipisci veritatis perferendis laborum.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, aliquam? Odit reprehenderit dicta illo asperiores exercitationem velit quis voluptate veniam beatae, quae maxime, iste dolore, nulla minima porro quo totam fuga eveniet neque praesentium nobis! Placeat non pariatur suscipit. Officiis eveniet consequatur rerum magnam alias necessitatibus earum quidem incidunt soluta reprehenderit veniam, exercitationem nam explicabo dolor autem sunt eum ab est deserunt placeat. Odio est ducimus aliquam consectetur dolore mollitia ipsa minima, praesentium reiciendis vero necessitatibus nemo error quas facere animi voluptas cum deleniti, atque, earum enim ex? Ea dolorem incidunt nam consectetur id. Ullam dicta adipisci veritatis perferendis laborum.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
