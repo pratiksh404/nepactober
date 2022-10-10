91b1d32cdb57dc5da1f7fe2f46f7cf274dd4ffab
Parjanya Shakya <parjanya57@gmail.com>
2022-10-10 14:26:42 +0545

    Nodejs Basics(intro,globals,names,utils)

---
+++ Nodejs/Introduction/2-globals.js
@@ -0,0 +1,12 @@
+// Globals - NO Window
+
+//__dirname - path to current directory
+// __filename - filename
+// require -function to use modules (Common Js)
+// module - info about current module (file)
+//process - info about env where the program is being executed
+
+console.log(__dirname,__filename);
+setInterval(() =>{
+    console.log("Hello World")
+},1000)
---
+++ Nodejs/Introduction/3.1-names.js
@@ -0,0 +1,7 @@
+//local
+const secret = 'SUPER SECRET'
+// shared
+const john = 'John'
+const peter = "Peter"
+
+module.exports = {john,peter}
