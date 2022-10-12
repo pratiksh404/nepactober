cf090c43323626742be471a2a967b5705ef41350
devildaikells <91599631+devildaikells@users.noreply.github.com>
2022-10-12 21:28:43 +0545

    doubletoint.java

---
+++ doubletoint.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+
+    // create double variables
+    double a = 23.78D;
+    double b = 52.11D;
+
+    // convert double into int
+    // using typecasting
+    int c = (int)a;
+    int d = (int)b;
+
+    System.out.println(c);    // 23
+    System.out.println(d);    // 52
+  }
+}
