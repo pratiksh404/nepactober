87bff42c1ecd8a3f95761d6de142edf9a089e69d
Deelashatdr <91687872+Deelashatdr@users.noreply.github.com>
2022-10-17 12:58:48 +0545

    removal.py

---
+++ Python/removal.py
@@ -0,0 +1,35 @@
+# Python program to demonstrate
+# Removal of elements in a Array
+
+# importing "array" for array operations
+import array
+
+# initializing array with array values
+# initializes array with signed integers
+arr = array.array('i', [1, 2, 3, 1, 5])
+
+# printing original array
+print ("The new created array is : ", end ="")
+for i in range (0, 5):
+	print (arr[i], end =" ")
+
+print ("\r")
+
+# using pop() to remove element at 2nd position
+print ("The popped element is : ", end ="")
+print (arr.pop(2))
+
+# printing array after popping
+print ("The array after popping is : ", end ="")
+for i in range (0, 4):
+	print (arr[i], end =" ")
+
+print("\r")
+
+# using remove() to remove 1st occurrence of 1
+arr.remove(1)
+
+# printing array after removing
+print ("The array after removing is : ", end ="")
+for i in range (0, 3):
+	print (arr[i], end =" ")
