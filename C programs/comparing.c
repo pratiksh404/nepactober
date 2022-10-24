4d528f6316ca538232f08e2a45b96cf942eba6a2
haza10 <116536417+haza10@users.noreply.github.com>
2022-10-24 09:50:32 +0545

    comparing.c

---
+++ C programs/comparing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int *ptrA,*ptrB;
+
+    ptrA = (int *)1;
+    ptrB = (int *)2;
+    
+    if(ptr2 > ptr1)
+        printf("PtrB is greater than ptrA");
+
+return(0);
+}
